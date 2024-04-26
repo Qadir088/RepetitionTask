f819edaebf8deaf7bdbf21039a04729289914014
Qadir088 <qadircabbarli55@gmail.com>
2024-04-26 13:24:49 +0400

    Repetiton Task

---
+++ RepetitionMethodsTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionMethodsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = {"Baki", "Sumgayit","Gence"};
+            Console.WriteLine("Please enter a char");
+            string input = Console.ReadLine();
+            char c = Convert.ToChar(input);
+
+            foreach (string word in arr)
+            {
+                foreach (char letter in word)
+                {
+                    if(c == letter)
+                    {
+                        Console.WriteLine("same");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Same");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
